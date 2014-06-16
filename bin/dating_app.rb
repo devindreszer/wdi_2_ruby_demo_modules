d876f011d969b5e48b1bb06bc32c57f37b21543c
Devin Dreszer <devindreszer@gmail.com>
2014-06-16 11:30:38 -0400

    Add module to create namespace for dating app

--- bin/dating_app.rb
+++ bin/dating_app.rb
@@ -1,19 +1,25 @@
+require 'pry'
 require 'date'
 
-class Date
-  attr_accessor :description, :date_day
 
-  def initialize(description)
-    @description = description
-  end
-  def date_day=(date)
-    @date_day = date 
-  end
+module MyDatingApp
+  class Date
+    attr_accessor :description, :date_day
 
+    def initialize(description)
+      @description = description
+    end
+    def date_day=(date)
+      @date_day = date
+    end
+
+  end
 end
 
+# binding.pry
+
 # Oh no, not only a bad date but my app is broken!
-bad_date = Date.new('Jeeeeeerk')
+bad_date = MyDatingApp::Date.new('Jeeeeeerk')
 
 # last saturday
 bad_date.date_day = Date.strptime('06-14-2014', '%m-%d-%Y')
